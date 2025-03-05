ef5cdb2d6d7bf5e1c9b5a6e5cf3e0100897629ec
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-05 09:23:03 +0530

    make changes of using a while loop and a flag variable

--- src/main/java/employeeDriver/EmpDriver.java
+++ src/main/java/employeeDriver/EmpDriver.java
@@ -1,57 +1,66 @@
 package employeeDriver;
 
 import java.util.Scanner;
-import employeeService.EmpService;//importing the EmpService class from it's package
+//importing the EmpService class from it's package to access there classes
+import employeeService.EmpService;
 
 //implementation class
 public class EmpDriver {
 	public static void main(String[] args) {
-		System.out.println("=========== WELCOME TO EMPLOYEE MANAGEMENT APPLICATION ============");
-		System.out.println("\nMENU DETAILS :---");
-		System.out.println("1. Press 1 to Register Employee. \n2. Press 2 to Update Employee. \n3. Press 3 to Delete Employee. \n4. Press 4 to Fetch Employee Data(All). \n5. Press 5 to Close the Application.");
-		Scanner sc = new Scanner(System.in);
-		System.out.print("\nEnter Your Choice: ");
-		int choice = sc.nextInt();
+		System.out.print("=========== WELCOME TO EMPLOYEE MANAGEMENT APPLICATION ============");
 		
-		EmpService e1 = new EmpService();
+		boolean flag = true;
+		//until the user press 5to close the application the driver menu will be shown to the user
+		while(flag) {
+			System.out.println("\nMENU DETAILS :");
+			System.out.println("~~~~~~~~~~~~~~");
+			System.out.println("Press 1 to Register Employee. \nPress 2 to Update Employee. \nPress 3 to Delete Employee. \nPress 4 to Fetch Employee Data(All). \nPress 5 to Close the Application.");
+			Scanner sc = new Scanner(System.in);
+			System.out.print("\nEnter Your Choice: ");
+			int choice = sc.nextInt();//storing the user input choice
 		
-		switch (choice) {
-		case 1:
-			int res = e1.save();
-			if(res!=0) {
-				System.out.print("Employee Registration Successful..✅");
-			}else {
-				System.out.print("Employee Registration Unsuccessful..❎");
+		    EmpService e1 = new EmpService();
+		    
+		    switch (choice) {
+			case 1:
+				int res = e1.save();
+				if(res!=0) {
+					System.out.println("Employee Registration Successful.✅");
+				}else {
+					System.out.println("Employee Registration Unsuccessful.❎");
+				}
+				break;
+			case 2:
+				int uRes = e1.update();
+				if(uRes != 0) {
+					System.out.println("Employee Data Updated..!🆗");
+				}else {
+					System.out.println("Employee Data not Updated...🔀");
+				}
+				break;
+			case 3:
+				int dRes = e1.delete();
+				if(dRes != 0) {
+					System.out.println("Employee Data Deleted Successfully..!✔");
+				}else {
+					System.out.println("Employee Data Does not Deleted...✖");
+				}
+				break;
+			case 4:
+				e1.fetchAll();
+				System.out.println("All Employees Data Fetched Successfully..!");
+				break;
+			case 5:
+				e1.exit();
+				flag = false;
+				System.out.println("Exiting the application. Thank You..💌:)");
+				sc.close();
+				break;
+			default:
+				System.out.println("Enter a valid choice between: 1 to 5");
+				break;
 			}
-			break;
-		case 2:
-			int uRes = e1.update();
-			if(uRes != 0) {
-				System.out.println("Employee Data Updated..!🆗");
-			}else {
-				System.out.println("Employee Data not Updated...🔀");
-			}
-			break;
-		case 3:
-			int dRes = e1.delete();
-			if(dRes != 0) {
-				System.out.println("Employee Data Deleted Successfully..!✔");
-			}else {
-				System.out.println("Employee Data Does not Deleted...✖");
-			}
-			break;
-		case 4:
-			e1.fetchAll();
-			System.out.println("All Employees Data Fetched Successfully..!");
-			break;
-		case 5:
-			e1.exit();
-			System.out.println("Exiting the application. Thank You..💌:)");
-			break;
-		default:
-			System.out.println("Enter a valid choice between: 1 to 5");
-			break;
+		    
 		}
-		sc.close();
 	}
 }
